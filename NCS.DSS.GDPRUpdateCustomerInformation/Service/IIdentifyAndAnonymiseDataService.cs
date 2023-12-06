82b083ace59df2df672140e64d5856edd621d92f
hnandra <32364605+hnandra@users.noreply.github.com>
2023-12-06 17:27:45 +0000

    Adding service file for both functions

---
+++ NCS.DSS.GDPRUpdateCustomerInformation/Service/IIdentifyAndAnonymiseDataService.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace NCS.DSS.GDPRUpdateCustomerInformation.Service
+{
+    public interface IIdentifyAndAnonymiseDataService
+    {
+        Task AnonymiseData();
+        Task IdentifyCustomers();
+    }
+}
---
+++ NCS.DSS.GDPRUpdateCustomerInformation/Service/IdentifyAndAnonymiseDataService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using NCS.DSS.GDPRUpdateCustomerInformation.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCS.DSS.GDPRUpdateCustomerInformation.Service
+{
+    public class IdentifyAndAnonymiseDataService : IIdentifyAndAnonymiseDataService
+    {
+        private readonly string _GDPRUpdateCustomersStoredProcedureName = Environment.GetEnvironmentVariable("GDPRUpdateCustomersStoredProcedureName");
+        private readonly string _GDPRIdentifyCustomersStoredProcedureName = Environment.GetEnvironmentVariable("GDPRIdentifyCustomersStoredProcedureName");
+
+        private readonly IAzureSqlDbProvider _azureSqlDbProvider;
+        private readonly ILogger<IdentifyAndAnonymiseDataService> _logger;
+
+        public IdentifyAndAnonymiseDataService(
+            AzureSqlDbProvider azureSqlDbProvider,
+            ILogger<IdentifyAndAnonymiseDataService> logger)
+        {
+            _azureSqlDbProvider = azureSqlDbProvider;
+            _logger = logger;
+        }
+
+        public async Task AnonymiseData()
+        {
+            _azureSqlDbProvider.ExecuteStoredProcedure(_GDPRUpdateCustomersStoredProcedureName);
+        }
+
+        public async Task IdentifyCustomers()
+        {
+            _azureSqlDbProvider.ExecuteStoredProcedure(_GDPRIdentifyCustomersStoredProcedureName);
+        }
+    }
+}

ff3a6c2101db4ef2e73d83455b92eefd7378a08f
hnandra <32364605+hnandra@users.noreply.github.com>
2023-12-07 15:25:16 +0000

    Adding startup file

---
+++ NCS.DSS.GDPRUpdateCustomerInformation/startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NCS.DSS.GDPRUpdateCustomerInformation;
+using NCS.DSS.GDPRUpdateCustomerInformation.DB;
+using NCS.DSS.GDPRUpdateCustomerInformation.Service;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace NCS.DSS.GDPRUpdateCustomerInformation
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging();
+            builder.Services.AddSingleton<IAzureSqlDbProvider, AzureSqlDbProvider>();
+            builder.Services.AddSingleton<IIdentifyAndAnonymiseDataService, IdentifyAndAnonymiseDataService>();
+        }
+    }
+}

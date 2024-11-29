6d26a85bc127c4c2582ff84644bbc3164cad483d
Joe Hudson-Small <Joehs@me.com>
2024-11-29 15:49:53 +0000

    updates parsing

--- NCS.DSS.GDPRUpdateCustomerInformation/Services/CosmosDBService.cs
+++ NCS.DSS.GDPRUpdateCustomerInformation/Services/CosmosDBService.cs
@@ -113,22 +113,19 @@ namespace NCS.DSS.DataUtility.Services
         {
             _logger.LogInformation($"Attempting to retrieve Cosmos records/documents with value '{value}' for field '{field}' from container '{containerName}' from within database '{databaseName}'");
 
-            if (int_bool)
-            {
-                if (int.TryParse(value, out int intValue))
-                {
-                    value = intValue.ToString();
-                }
-                else
-                {
-                    _logger.LogError($"Failed to convert value '{value}' to an integer.");
-                    return;
-                }
-            }
-
             Container cosmosDbContainer = _cosmosDbClient.GetContainer(databaseName, containerName);
 
-            string queryString = $"SELECT * FROM c WHERE c.{field} = @value";
+            // handles string/int parsing based on the int_bool flag
+            string queryString;
+            if (int_bool)
+            {
+                queryString = $"SELECT * FROM c WHERE c.{field} = {value}"; 
+            }
+            else
+            {
+                queryString = $"SELECT * FROM c WHERE c.{field} = @value";
+            }
+
             QueryDefinition queryDefinition = new QueryDefinition(queryString).WithParameter("@value", value);
 
             FeedIterator<dynamic> resultSet = cosmosDbContainer.GetItemQueryIterator<dynamic>(queryDefinition);
